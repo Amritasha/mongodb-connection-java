2d6b23418124f2b66b9a7de83b0ab1c498c00c75
amritasha <amritasha71@gmail.com>
2017-07-05 23:06:12 +0530

    connection successful

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            MongoClient mc = new MongoClient("localhost", 27017);
+
+            DB db = mc.getDB("test");
+            System.out.println("Connected successfully");
+        }catch(Exception e){
+            System.err.println(e.getClass().getName()+" "+e.getMessage());
+        }
     }
 }
