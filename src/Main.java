f1627a4ec57a859b2657f621eca4ac66cf3d935c
amritasha <amritasha71@gmail.com>
2017-07-05 23:28:09 +0530

    insert and view

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-import com.mongodb.DB;
-import com.mongodb.MongoClient;
+import com.mongodb.*;
+
+import java.util.*;
 
 public class Main {
 
@@ -7,8 +8,31 @@ public class Main {
         try {
             MongoClient mc = new MongoClient("localhost", 27017);
 
-            DB db = mc.getDB("test");
+           DB db = mc.getDB("test");
             System.out.println("Connected successfully");
+
+            List<String> dbs = mc.getDatabaseNames();
+            for(String db_name: dbs){
+                System.out.println(db_name);
+            }
+
+            Set<String> tables = db.getCollectionNames();
+            for(String col : tables){
+                System.out.println("col");
+            }
+
+            DBCollection dbc = db.getCollection("new");
+            /*BasicDBObject bdb = new BasicDBObject();
+            bdb.put("first_name","amritasha");
+            bdb.put("last_name","agrawal");
+            bdb.put("date",new Date());
+            dbc.insert(bdb);*/
+
+            DBCursor cursor = dbc.find();
+            while(cursor.hasNext()){
+                System.out.println(cursor.next());
+            }
+
         }catch(Exception e){
             System.err.println(e.getClass().getName()+" "+e.getMessage());
         }
